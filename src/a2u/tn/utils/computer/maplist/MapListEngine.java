a28b225f9a594f61241f3e81ddc64b2f000a0205
anymaker <a2uqwertyu@gmail.com>
2019-12-18 12:05:31 +1000

    Deleted obsolete code

--- src/a2u/tn/utils/computer/maplist/MapListEngine.java
+++ src/a2u/tn/utils/computer/maplist/MapListEngine.java
@@ -1,37 +1,6 @@
 package a2u.tn.utils.computer.maplist;
 
 import a2u.tn.utils.computer.calcobj.ObjCalcEngine;
-import a2u.tn.utils.computer.calculator.Calculator;
-import a2u.tn.utils.computer.maplist.functions.Count;
-import a2u.tn.utils.computer.maplist.functions.DateToStr;
-import a2u.tn.utils.computer.maplist.functions.Distinct;
-import a2u.tn.utils.computer.maplist.functions.False;
-import a2u.tn.utils.computer.maplist.functions.First;
-import a2u.tn.utils.computer.maplist.functions.Last;
-import a2u.tn.utils.computer.maplist.functions.Like;
-import a2u.tn.utils.computer.maplist.functions.Max;
-import a2u.tn.utils.computer.maplist.functions.MaxInRows;
-import a2u.tn.utils.computer.maplist.functions.Min;
-import a2u.tn.utils.computer.maplist.functions.Null;
-import a2u.tn.utils.computer.maplist.functions.Nvl;
-import a2u.tn.utils.computer.maplist.functions.RowNum;
-import a2u.tn.utils.computer.maplist.functions.RowsCount;
-import a2u.tn.utils.computer.maplist.functions.True;
-import a2u.tn.utils.computer.maplist.types.TBool;
-import a2u.tn.utils.computer.maplist.types.TDate;
-import a2u.tn.utils.computer.maplist.types.TDouble;
-import a2u.tn.utils.computer.maplist.types.TFloat;
-import a2u.tn.utils.computer.maplist.types.TInt;
-import a2u.tn.utils.computer.maplist.types.TList;
-import a2u.tn.utils.computer.maplist.types.TLong;
-import a2u.tn.utils.computer.maplist.types.TMap;
-import a2u.tn.utils.computer.maplist.types.TString;
-import a2u.tn.utils.computer.maplist.functions.ToDate;
-
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Calculator with extract values by code from source such as Map or List with Map
