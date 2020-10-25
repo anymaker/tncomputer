2d9cf1c4c192e7ca3a860b0ce484fb0e5e531a54
anymaker <a2uqwertyu@gmail.com>
2020-10-25 09:03:52 +0300

    New function ToString

---
+++ src/a2u/tn/utils/computer/calcobj/functions/string/ToString.java
@@ -0,0 +1,33 @@
+package a2u.tn.utils.computer.calcobj.functions.string;
+
+import a2u.tn.utils.computer.calculator.Calculator;
+import a2u.tn.utils.computer.calculator.Function;
+import a2u.tn.utils.computer.formula.FormulaPart;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Convert any value to String
+ */
+public class ToString extends Function {
+
+  public ToString(Calculator calculator) {
+    super(calculator);
+  }
+
+  @Override
+  protected List<Parameter> initParameters() {
+    List<Parameter> parameters = new ArrayList<>();
+    parameters.add(new Parameter(Object.class, "object"));
+    return parameters;
+  }
+
+  @Override
+  public Object run(List<FormulaPart> params, Object row, int rowIndex, Collection<Object> allRows) {
+    Object object_ = calculator.calcArgument(params.get(0), row, rowIndex, allRows);
+    String object = calculator.toType(String.class, object_);
+    return object;
+  }
+}
